2184073a279f0bb2dd428d66290f67818e63c672
mr904i <hashimoto_keisuke@hashi-MBP.local>
2021-04-04 13:00:55 +0900

    add page components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Index from './containers/users/Index';
 import SignUp from './containers/users/SignUp';
+import SignIn from './containers/users/SignIn';
+import UserList from './containers/users/UserList';
+import Profile from './containers/users/Profile';
 
 const App = () => {
   return(
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={SignUp} />
+        <Route exact path="/" component={Index} />
+        <Route exact path="/sign_up" component={SignUp} />
+        <Route exact path="/sign_in" component={SignIn} />
+        <Route exact path="/users" component={UserList} />
+        <Route exact path="/users/profile" component={Profile} />
       </Switch>
     </BrowserRouter>
   )
